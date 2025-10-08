0ccfed908bf41998f5dbdb923daebed06f3491ab
zhonggruii <leezhongrui1@gmail.com>
2025-10-08 23:31:37 +0800

    Reorder imports

--- src/main/java/seedu/address/model/person/Student.java
+++ src/main/java/seedu/address/model/person/Student.java
@@ -1,22 +1,22 @@
 package seedu.address.model.person;
 
-import seedu.address.model.tag.Tag;
-
 import java.util.Set;
 
+import seedu.address.model.tag.Tag;
+
 /**
- *  Creates student object
+ * Creates student object.
  */
 public class Student extends Person {
 
     /**
      * Every field must be present and not null.
      *
-     * @param name
-     * @param phone
-     * @param email
-     * @param address
-     * @param tags
+     * @param name Name of the student
+     * @param phone Phone number of the student
+     * @param email Email of the student
+     * @param address Address of the student
+     * @param tags Tags associated with the student
      */
     public Student(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags);
