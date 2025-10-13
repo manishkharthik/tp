de54b4853be73b8f6fc0b3fdef26f86d3f005a32
Anthony Chow <wjchowanthony@gmail.com>
2025-10-14 00:23:13 +0800

    Remove unecessary imports in attendanceRecordTest.java

--- src/test/java/seedu/address/model/attendance/AttendanceListTest.java
+++ src/test/java/seedu/address/model/attendance/AttendanceListTest.java
@@ -1,20 +1,13 @@
 package seedu.address.model.attendance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import seedu.address.model.attendance.AttendanceList;
 
 public class AttendanceListTest {
 
