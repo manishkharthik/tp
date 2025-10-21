16c843d5bf1b5e0f75a81f2e9d42d379b021384f
Anthony Chow <wjchowanthony@gmail.com>
2025-10-21 12:46:41 +0800

    Refactor(attendance): make AttendanceRecord use Lesson+Status (remove dateTime identity)

--- src/main/java/seedu/address/model/attendance/AttendanceRecord.java
+++ src/main/java/seedu/address/model/attendance/AttendanceRecord.java
@@ -1,7 +1,11 @@
 package seedu.address.model.attendance;
 
+import static java.util.Objects.requireNonNull;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
+
+import seedu.address.model.lesson.Lesson;
 
 
 /**
@@ -13,15 +17,17 @@ import java.util.Objects;
  */
 public class AttendanceRecord {
     // Private fields
-    private final LocalDateTime dateTime;
+    private final Lesson lesson;
     private AttendanceStatus status;
 
     /**
      * Constructor
      */
-    public AttendanceRecord(AttendanceStatus status, LocalDateTime dateTime) {
+    public AttendanceRecord(Lesson lesson, AttendanceStatus status) {
+        requireNonNull(lesson);
+        requireNonNull(status);
+        this.lesson = lesson;
         this.status = status;
-        this.dateTime = dateTime;
     }
 
     /**
@@ -41,36 +47,29 @@ public class AttendanceRecord {
     /**
      * @return returns current date and time of this attendance record
      */
-    public LocalDateTime getDateTime() {
-        return this.dateTime;
+    public Lesson getLesson() {
+        return lesson;
     }
 
     /**
      * Returns true if attendance record already exists
-     * Uniqueness is determined by dateTime only
-     * (i.e. same dateTime)
+     * Uniqueness is determined by lesson only
+     * (i.e. same lesson)
      *
      * @param other the other attendance record to compare with
-     * @return true if both attendance records have the same dateTime
+     * @return true if both attendance records have the same lesson
      */
     @Override
     public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        // Instanceof handles nulls
-        if (!(other instanceof AttendanceRecord)) {
-            return false;
-        }
-
-        AttendanceRecord otherAttendanceRecord = (AttendanceRecord) other;
-        return dateTime.equals(otherAttendanceRecord.dateTime);
+        if (other == this) return true;
+        if (!(other instanceof AttendanceRecord)) return false;
+        AttendanceRecord o = (AttendanceRecord) other;
+        return lesson.equals(o.lesson);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(dateTime);
+        return Objects.hash(lesson, status);
     }
 
     @Override
