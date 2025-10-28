ee12a94f2bb3eb234a6c308fd2b067d59be1ffe4
manishkharthik <manishkharthik0630@gmail.com>
2025-10-28 21:30:11 +0800

    Modify listattendancecommand parser

--- src/test/java/seedu/address/logic/parser/ListAttendanceCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListAttendanceCommandParserTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
