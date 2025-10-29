8a12e136129873e93b65f26e65e36b7e280fb1f9
darrensimmx <darrensimmx@gmail.com>
2025-10-29 16:43:57 +0800

    Fix: List Command so that they have to type list

---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parse the List command.
+ */
+public class ListCommandParser implements Parser<ListCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListCommand
+     * and returns a ListCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format
+     */
+    @Override
+    public ListCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+
+        if (!trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    ListCommand.MESSAGE_USAGE));
+        }
+
+        return new ListCommand();
+    }
+}
